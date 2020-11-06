6799ca622fdd596ae2efeb90b7ed6d06508f31ef
denislgx <denislg_@hotmail.com>
2020-11-06 13:38:30 -0300

    Home Component > Banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import "./App.css";
 
 const App = () => {
     return (
         <div className="app">
             <Header />
-            {/* HOME */}
+            <Home />
         </div>
     );
 };
