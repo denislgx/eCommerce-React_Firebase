3473f819cfa311ee8875b9867e47e61b1ce62c26
denislgx <denislg_@hotmail.com>
2020-11-16 12:44:39 -0300

    Orders Component under construction

---
+++ src/components/Orders.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/Orders.css";
+
+const Orders = () => {
+    return (
+        <div className="orders">
+            <h1>ORDERSSSSSS</h1>
+        </div>
+    );
+};
+
+export default Orders;
